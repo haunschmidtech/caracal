ef88a57bdadca98c540711f5ffc26b45d9859944
ntrm <martin@haunschmid.technology>
2020-11-21 19:24:55 +0100

    newline hopefully

--- lib/caracal/renderers/footer_renderer.rb
+++ lib/caracal/renderers/footer_renderer.rb
@@ -33,6 +33,16 @@ module Caracal
                   xml['w'].t({ 'xml:space' => 'preserve' }) do
                     xml.text "#{ document.page_number_label } "
                   end
+                end
+              end
+              unless document.page_number_label_2.nil?
+                xml['w'].r run_options do
+                  xml['w'].rPr do
+                    xml['w'].rStyle({ 'w:val' => 'PageNumber' })
+                    unless document.page_number_label_size.nil?
+                      xml['w'].sz({ 'w:val'  => document.page_number_label_size })
+                    end
+                  end
                   xml['w'].t({ 'xml:space' => 'preserve' }) do
                     xml.text "#{ document.page_number_label_2 } "
                   end
